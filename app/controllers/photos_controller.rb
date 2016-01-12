7763cd638eca78b55481060f8c70768381d89c78
Gary Carmichael <g.carmichael90@gmail.com>
2016-01-12 09:51:19 +0000

    Added more users, tested associations. Updated Friend index to show appropriate names. Added new/edit Photo views.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,5 +6,35 @@ class PhotosController < ApplicationController
     @photos = Photo.all
     @users = User.all
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    current_user.photos.create(photo_params)
+    redirect_to(photos_index_path)
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    photo = Photo.find(params[:id])
+    photo.update(photo_params)
+    redirect_to(photos_path)
+  end
+
+  def destroy
+    photo = Photo.find(params[:id])
+    photo.destroy
+    redirect_to(photos_path)
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:title, :image, :description, :user_id)
+  end
   
 end
