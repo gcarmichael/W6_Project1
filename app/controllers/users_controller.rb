8691a097e858b8e74dc5fe771275729939fff540
Gary Carmichael <g.carmichael90@gmail.com>
2016-01-13 16:05:58 +0000

    Started CSS work. Added normalize.css and fixed User controller to not display empty grid for self.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,14 @@ class UsersController < ApplicationController
   def index
     @friends = current_user.friendslist(current_user.id)
     #@users = User.all
-    @users = User.order(name: :asc)
+    # @users = User.order(name: :asc)
+    @other_users = get_other_users
+  end
 
+  def get_other_users
+    # id = current_user.id
+    # @users = User.find_by_sql("SELECT * FROM users WHERE users.id != ?", id)
+    @users = User.where.not(id: current_user.id).order(name: :asc)
   end
 
 end
